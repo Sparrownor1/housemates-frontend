22a2e9e2fe4c1a84ef977d657ee603c1020fd1e1
Deep Patel <dmpatel@andrew.cmu.edu>
2022-07-24 11:58:20 -0400

    implemented login input

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Login from "./components/Login";
 
 
 function App() {
   return (
-    <div className="App">
-      Housemates Login Initially
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path = "/" element={<div>None</div>}/>
+          <Route path = "/login" element={<Login/>}/>
+          <Route path = "/about" element={<div>About Page</div>}/>
+        </Routes>
+      </BrowserRouter>
   );
 }
 
@@ -16,4 +23,10 @@ TODO:
   2. figure out how typescript goes with reacts jsx or whatever
   3. figure out how the typescript compiles down to javascript in the context of a react project (try to do it on save)
   4. create a simple login form and figure out how to send the get request to the backend
+
+  FC is a builtin react type that can be imported from react
+    - defaults to children prop
+    - use an interface to describe the properties provided (FC<Interface>)
+    - use to guarantee that when that component is used, the prop is provided with it
+    - never use any in docs
 */
---
+++ src/components/Login.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+
+const Login: FC = () => {
+    return (
+    <div>
+      <input />
+      <input /> 
+    </div>)
+}
+
+export default Login
