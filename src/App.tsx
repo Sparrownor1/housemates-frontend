41ca3968d8ed0744f0bfab43d75fc744560f910e
Deep Patel <dmpatel@andrew.cmu.edu>
2022-07-24 10:51:06 -0400

    initial file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
+
 function App() {
   return (
     <div className="App">
-      Nos
+      Housemates Login Initially
     </div>
   );
 }
 
 export default App;
+
+/*
+TODO:
+  2. figure out how typescript goes with reacts jsx or whatever
+  3. figure out how the typescript compiles down to javascript in the context of a react project (try to do it on save)
+  4. create a simple login form and figure out how to send the get request to the backend
+*/
